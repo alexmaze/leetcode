f97aea627b8fd51918ca528581b2dceb6b033026
Alex (Jinghao) Yan <yanhao1991@gmail.com>
2017-04-30 11:38:37 +0800

    fix npm run template issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     "rules": {
         "camelcase": 0,
         "no-unused-vars": 0,
-        "space-before-function-paren": 0
+        "space-before-function-paren": 0,
+        "quotes": ["error", "double"]
     }
 };
--- utils/create.js
+++ utils/create.js
@@ -1,25 +1,31 @@
-const fs = require('fs')
-const path = require('path')
-const LeetCode = require('./leetcode')
-const ora = require('ora')
-const colors = require('colors')
-const moment = require('moment')
+const fs = require("fs")
+const path = require("path")
+const LeetCode = require("./leetcode")
+const ora = require("ora")
+const colors = require("colors")
+const moment = require("moment")
 
 let problemId = process.argv[2]
 if (!problemId) {
-  console.log(colors.cyan('# 请指定 LeetCode 题号'))
+  console.log(colors.cyan("# 请指定 LeetCode 题号"))
   process.exit(0)
 }
 problemId = parseInt(problemId)
 
-const span = ora(colors.yellow('# 加载中...'))
+const span = ora(colors.yellow("# 加载中..."))
 span.start()
 const leetcode = new LeetCode()
 
 leetcode.getList().then(data => {
   span.stop()
-  console.log('# 加载完成'.yellow, data.length)
-  writeTemplate(data[problemId - 1])
+  console.log("# 加载完成".yellow, data.length)
+  for (let item of data) {
+    if (item.stat.question_id === problemId) {
+      writeTemplate(item)
+      return
+    }
+  }
+  console.log("# did not find this question")
 })
 
 function writeTemplate (data) {
@@ -30,7 +36,7 @@ function writeTemplate (data) {
   const thePath = path.join(__dirname, `../src/${id}-${name}`)
   fs.access(thePath, err => {
     if (!err) {
-      console.error('已存在'.red)
+      console.error("已存在".red)
       process.exit(-1)
     }
     fs.mkdirSync(thePath)
@@ -39,12 +45,12 @@ function writeTemplate (data) {
 
 module.exports = 
 `
-    fs.writeFile(path.join(thePath, 'solution.js'), scriptTemp, err => {
+    fs.writeFile(path.join(thePath, "solution.js"), scriptTemp, err => {
       if (err) {
-        console.log('写文件失败'.red)
+        console.log("写文件失败".red)
         process.exit(-1)
       }
-      console.log('写文件成功'.green)
+      console.log("写文件成功".green)
     })
 
     const indexTemp = `/**
@@ -53,17 +59,17 @@ module.exports =
  * @url: https://leetcode.com/problems/${name}/#/description
  *
  * @by: Alex Yan <yanhao1991@gmail.com>
- * @date: ${moment().format('YYYY-MM-DD')}
+ * @date: ${moment().format("YYYY-MM-DD")}
 */
 
 const solution = require('./solution')
 `
-    fs.writeFile(path.join(thePath, 'index.js'), indexTemp, err => {
+    fs.writeFile(path.join(thePath, "index.js"), indexTemp, err => {
       if (err) {
-        console.log('写文件失败'.red)
+        console.log("写文件失败".red)
         process.exit(-1)
       }
-      console.log('写文件成功'.green)
+      console.log("写文件成功".green)
     })
   })
 }
